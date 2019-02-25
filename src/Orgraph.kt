6dda596200b968aa304e9965ab088ae82701a6a3
Dasha Fedorova <poooroh@mail.ru>
2019-02-25 21:59:50 +0300

    to change the name is hard :C

--- src/Orgraph.kt
+++ src/Orgraph.kt
@@ -1,35 +1,53 @@
 class Graph {
     var numOfVertices = 0
 
-    inner class Vertex(val name: String,) {
+    private val vertices = mutableMapOf<String, Vertex>()
+
+    inner class Vertex(val name: String) {
         val neighborsIn = mutableMapOf<Vertex, Int>()
         val neighborsOut = mutableMapOf<Vertex, Int>()
         val countingNumber = numOfVertices
     }
 
-    private val vertices = mutableMapOf<String, Vertex>()
-
-    private operator fun get(name: String): Vertex = vertices[name] ?: throw IllegalArgumentException()
+    private operator fun get(name: String) = vertices[name] ?: throw IllegalArgumentException()
 
     fun addVertex(name: String) {
         vertices[name] = Vertex(name)
         numOfVertices++
     }
 
-    fun deleteVertex(name: String) {}
+    fun deleteVertex(name: String) {
+        numOfVertices--
+    }
 
-    fun changeName(name: String){}
+    fun changeName(name: String, newName: String) {
+        if(newName in vertices.keys || name !in vertices.keys) throw IllegalArgumentException()
+        else {
+            vertices[newName] = vertices[name]!!
+            for (neighbour in vertices[newName]!!.neighborsIn.keys) {
+                neighbour.neighborsOut.remove(vertices[name])
+                neighbour.neighborsOut[vertices[newName]!!] = getWeight(neighbour.name, newName)
+            }
+            for (neighbour in vertices[newName]!!.neighborsOut.keys) {
+                neighbour.neighborsIn.remove(vertices[name])
+                neighbour.neighborsOut[vertices[name]!!] = getWeight(newName, neighbour.name)
+            }
+                vertices.remove(name)
+        }
+    }
 
     fun addEdge(nameFrom: String,nameTo: String) {}
 
     fun changeWeight(nameFrom: String, nameTo: String) {}
+
+    fun getWeight(nameFrom: String, nameTo: String) = vertices[nameFrom]!!.neighborsOut[vertices[nameTo]]!!
 
     private fun connect(first: Vertex, second: Vertex, weight: Int) {
         first.neighborsOut[second] = weight
         second.neighborsIn[first] = weight
     }
 
-    fun connect(first: String, second: String) = connect(this[first], this[second]){}
+    fun connect(first: String, second: String, weight: Int) = connect(this[first], this[second], weight)
 
     fun deleteEdge(nameFrom: String, nameTo: String) {}
 
